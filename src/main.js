a3961f824e0251b9b764dda46f2a4f922b1a87ea
Yasuhiro Osajima <yasuhiro-osajima@gmo.jp>
2018-11-18 16:07:03 +0900

    initial commit.

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
 import Vue from 'vue'
+import 'material-icons/iconfont/material-icons.css'
+import './plugins/vuetify'
 import App from './App.vue'
 import router from './router'
-import store from './store'
+//import store from './store'
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
-  store,
+  //store,
   render: h => h(App)
 }).$mount('#app')
